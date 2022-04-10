5e61b4e5e4d1af34bf8a262b43a11881907e8e1e
xlii0239 <xlii0239@student.monash.edu>
2022-04-11 09:37:10 +1000

    auto complete function add

---
+++ FeelingArts/ClientApp/src/components/a17components/autocomplete/AutoComplete.js
@@ -0,0 +1,21 @@
+﻿import Autocomplete from "react-autocomplete";
+
+
+function AutoComplete() {
+    let value = [];
+
+    return (
+        <Autocomplete
+            getItemValue={(item) => item.label}
+            items={clist}
+            renderItem={(item, isHighlighted) =>
+                <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                    {item.label}
+                </div>
+            }
+            value={value}
+            onChange={(e) => value = e.target.value}
+            onSelect={(val) => value = val}
+        />
+    );
+}
