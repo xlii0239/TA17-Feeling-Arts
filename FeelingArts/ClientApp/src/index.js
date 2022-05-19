0e6761d6f3f261fd416b6c580ad9111e6654a311
xlii0239 <xlii0239@student.monash.edu>
2022-05-20 00:18:42 +1000

    delete unnessary files

--- FeelingArts/ClientApp/src/index.js
+++ FeelingArts/ClientApp/src/index.js
@@ -10,27 +10,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 import Index from "views/Index.js";
-import Sections from "views/Sections.js";
-import Presentation from "views/Presentation.js";
-import AboutUs from "views/examples/AboutUs.js";
-import AccountSettings from "views/examples/AccountSettings.js";
-import BlogPost from "views/examples/BlogPost.js";
-import BlogPosts from "views/examples/BlogPosts.js";
-import ChatPage from "views/examples/ChatPage.js";
-import CheckoutPage from "views/examples/CheckoutPage.js";
-import ContactUs from "views/examples/ContactUs.js";
-import Ecommerce from "views/examples/Ecommerce.js";
-import Error from "views/examples/Error.js";
-import Error500 from "views/examples/Error500.js";
-import InvoicePage from "views/examples/InvoicePage.js";
-import LandingPage from "views/examples/LandingPage.js";
-import LoginPage from "views/examples/LoginPage.js";
-import PricingPage from "views/examples/PricingPage.js";
-import ProductPage from "views/examples/ProductPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import ResetPage from "views/examples/ResetPage.js";
-
 
 import HomeBlog from "views/HomeBlog.js";
 //import Search from "views/Search.js";
