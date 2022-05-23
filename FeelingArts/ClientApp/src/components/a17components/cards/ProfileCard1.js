c073a83f31021b43d26645df1b02d2374ed1f9a1
DanyueF <89259194+DanyueF@users.noreply.github.com>
2022-05-23 13:09:02 +1000

    front-update for icons

--- FeelingArts/ClientApp/src/components/a17components/cards/ProfileCard1.js
+++ FeelingArts/ClientApp/src/components/a17components/cards/ProfileCard1.js
@@ -7,6 +7,8 @@ import {
   CardHeader,
   CardBody,
 } from "reactstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobe, faGem, faImages } from '@fortawesome/fontawesome-free-solid';
 
 // Core Components
 
@@ -54,7 +56,7 @@ function ProfileCard1(props) {
                 <div className="d-flex align-items-center">
                   <div>
                     <Badge className="badge-circle mr-3" color="info">
-                      <i className="ni ni-single-02"></i>
+                       <i><FontAwesomeIcon icon={faGlobe} alt="nationality" /></i>
                     </Badge>
                   </div>
                   <div>
@@ -66,7 +68,7 @@ function ProfileCard1(props) {
                 <div className="d-flex align-items-center">
                   <div>
                     <Badge className="badge-circle mr-3" color="success">
-                      <i className="ni ni-trophy"></i>
+                      <i><FontAwesomeIcon icon={faGem} alt="famous in" /></i>
                     </Badge>
                   </div>
                   <div>
@@ -78,7 +80,7 @@ function ProfileCard1(props) {
                 <div className="d-flex align-items-center">
                   <div>
                     <Badge className="badge-circle mr-3" color="warning">
-                                          <i className="ni ni-diamond"></i>
+                      <i><FontAwesomeIcon icon={faImages} alt="master piece" /></i>
                     </Badge>
                   </div>
                   <div>
